60140c1f17829109bd4c15db3a315ef259edd05a
Brown <257898@NTTDATA.COM>
2022-09-07 12:00:24 -0500

    updating redirect uri

--- src/app/config/ntt-okta-config.ts
+++ src/app/config/ntt-okta-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc: {
         clientId: '0oa6d44lwwDsEdFHH5d7',
         issuer: 'https://dev-39960447.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: window.location.origin + 'login/callback',
         pkce: true,
         scopes: [
             'openid',
